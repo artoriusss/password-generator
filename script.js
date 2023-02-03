2e017961d34f0904b555d309aec43f782346baea
Arthur <usoprotumblr@gmail.com>
2023-02-03 03:40:25 -0300

    main js script

---
+++ script.js
@@ -0,0 +1,31 @@
+let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*()_-+={[}]^?/:;.,'
+let newPasswd = [] // If this variable is a string instead of an array, it will return "undefined" for the first generation. It's a simple fix.
+
+const pwGenerate = document.getElementById('generate')
+const unorderedList = document.getElementById('list-passwds')
+
+pwGenerate.addEventListener('click', genPasswords)
+
+// Function to generate passwords
+function genPasswords(){
+    const passwd = document.getElementById('qtity').value
+    regex = /\D/g // Regex to discount any letters and symbols, I might update this.
+    let numPasswd = passwd.replace(regex, '')
+    Number(numPasswd)
+    if(numPasswd > 10 || numPasswd <= 0 || passwd == ""){
+        alert('Invalid input!')
+    } else{
+        unorderedList.innerHTML = ''
+        const charArr = char.split("")
+        // This first for loop adds a new password on the ul element.
+        for(let i = 0; i < numPasswd; i++){ 
+            // This second for loop runs through the "char" string to add a new random character into the "newPasswd" array. 
+            for(let j = 0; j < 9; j++){ 
+                let charIndex = Math.floor(Math.random() * charArr.length)
+                newPasswd += charArr[charIndex]
+            }
+        unorderedList.innerHTML += `<li>${newPasswd}</li>`
+        newPasswd = '' // Resets the ul every time the generate button is clicked.
+        }
+    }
+}
